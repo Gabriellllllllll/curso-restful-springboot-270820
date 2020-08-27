23ccd17cf163453f14c521c50e3a26f6400d398c
GabrielMaciel <gabrielmacielboituva19@gmail.com>
2020-08-27 20:21:37 -0300

    Primiero Commit

---
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClienteController {
+    
+    @GetMapping("/cliente")
+    public String getClientes(){
+        return "Vai algum dia retornar o BD todos os clientes!";
+    }
+    
+    @GetMapping("/cliente/{codigo}")
+    public String getCliente(@PathVariable int codigo){
+        if(codigo>0)
+            return "Vai algum dia retornar um cliente " + codigo;
+        else
+            return "Numero negativo, cliente nao encontrado " + codigo;
+    }
+}
